0766c87b4d60ed194a1329596f7af5d530c4d029
mylespollie <mylespollie@gmail.com>
2017-06-20 13:27:41 -0400

    Added functionality to output the image IDs to a file to allow for feeding images into a scanner.  Additionally added a line to remove duplicate image IDs to avoid redundant scanning

--- pullLocal.sh
+++ pullLocal.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+> imageIDs.txt
 REPOLIST="$(curl http://localhost:5000/v2/_catalog)" #Replace "localhost:5000 with your registry address"
 REPOLIST="$(echo ${REPOLIST#*[})";
 REPOLIST="$(echo ${REPOLIST%]*})";
@@ -15,4 +16,7 @@ do
 		j="${j#\"}";
 		docker pull localhost:5000/${i}:${j};
 	done
+	docker images localhost:5000/${i} --format "{{.ID}}" >> imageIDs.txt #Print images IDs to  file
+	OUTPUT="$(awk '!a[$0]++' imageIDs.txt)"								 #Remove duplicates image IDs
+	echo "${OUTPUT}" > imageIDs.txt;
 done
